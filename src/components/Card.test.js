b805a3dc1f5fa3967bfce4f9142e8ff544f5cb9b
Oskar Gawlak <oskargawlak@MacBook-Pro-jul.localdomain>
2019-11-13 09:55:15 +0100

    Created a simple test using match snapshot

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -4,5 +4,5 @@ import { shallow, mount, render } from "enzyme";
 import Card from "./Card";
 
 it("expect to render Card component", () => {
-  expect(shallow(<Card />).length).toEqual(1);
+  expect(shallow(<Card />)).toMatchSnapshot();
 });
